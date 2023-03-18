1cb2f10cc0845d6e29f8a42c7680fbee70c82d53
zhenxing huang <113489738+HZX-sparkle@users.noreply.github.com>
2023-03-18 13:50:22 +0800

    Update LogicPuzzle.py

--- LogicPuzzle.py
+++ LogicPuzzle.py
@@ -1,29 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define Knave 0
-#define Knight 1
-
-int main()
-{
-	int A = Knave;
-	int B = Knave;
-	int isFound = 0;
-	for (A = Knave; A <= Knight; A++)
-	{
-		for (B = Knave; B <= Knight; B++)
-		{
-			if (A ^ (B == Knight) == 0 && B ^ (A != B) == 0)
-			{
-				if (A) printf("A is a Knight\n");
-				else printf("A is a Knave\n");
-
-				if (B) printf("B is a Knight\n");
-				else printf("B is a Knave\n");
-				isFound++;
-			}
-		}
-	}
-
-	if (isFound == 0) printf("Sorry, there is no satisfied solution\n");
-}
+Knave=0
+Knight=1
+aSays=input("A says: \n")
+bSays=input("B says: \n")
+isFound=0
+A=Knave
+B=Knave
+for A in [0,1]:
+	for B in [0,1]:
+		if((A==Knight) == eval(aSays) and (B==Knight) == eval(bSays)):
+			print("case"+str(isFound+1))
+			if(A):
+				print("A is a Knight\n")
+			else:
+				print("A is a Knave\n")
+			if(B):
+				print("B is a Knight\n")
+			else:
+				print("B is a Knave\n")
+			isFound+=1
+if(isFound==0):
+	print("Sorry, there is no satisfied solution\n")
